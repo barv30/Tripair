dfc4c8f46e406f41f27b9efd2f7f92be7e36676e
barvaida <barv308@gmail.com>
2019-07-25 12:26:58 +0300

    enter email and password

--- app/src/main/java/com/example/tripair/MainActivity.java
+++ app/src/main/java/com/example/tripair/MainActivity.java
@@ -4,11 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private String userEmail;
+    private String userPassword;
+
 
     public void onSignInClicked(View v){
-    Log.i("Info","bla");
+
+       EditText userEmailObj = (EditText) findViewById(R.id.userEmail);
+       userEmail = userEmailObj.getText().toString();
+       EditText userPasswordObj= (EditText) findViewById(R.id.userPassword);
+       userPassword = userPasswordObj.getText().toString();
+       Log.i("Info","EMAIL:" + userEmail);
+       Log.i("Info","PASSWORD:" + userPassword);
     }
     
     @Override
