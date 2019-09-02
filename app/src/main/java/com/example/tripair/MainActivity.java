f817b516fb3851b1b2b26b998666c5c1f17b5488
barvaida <barv308@gmail.com>
2019-09-02 21:45:38 +0300

    fixed sign up and sign in if the email or password null

--- app/src/main/java/com/example/tripair/MainActivity.java
+++ app/src/main/java/com/example/tripair/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public void getEmailAndPassword ()
+    public boolean getEmailAndPassword ()
     {
         EditText userEmailObj = (EditText) findViewById(R.id.userEmail);
         userEmail = userEmailObj.getText().toString();
@@ -40,85 +40,107 @@ public class MainActivity extends AppCompatActivity {
         userPassword = userPasswordObj.getText().toString();
         Log.i("Info","EMAIL:" + userEmail);
         Log.i("Info","PASSWORD:" + userPassword);
+        if(userEmail.equals("") || userPassword.equals(""))
+        {
+            return false;
+        }
+        return true;
     }
 
     public void onSignInClicked(View v){
 
         DatabaseReference mRef =  database.getReference();
-        getEmailAndPassword();
-        m_Auth.signInWithEmailAndPassword(userEmail, userPassword)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.i("Info", "signInWithEmail:success");
-                            FirebaseUser user = m_Auth.getCurrentUser();
-                            String uid = user.getUid();
-                            ValueEventListener UserListener = new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    User user;
-                                    // Get Post object and use the values to update the UI
-                                    if (dataSnapshot.child("usersProfile").child(uid).exists()) {
-                                        user = dataSnapshot.child("usersProfile").child(uid).getValue(User.class);
-                                        openAllTripsActivity(uid, user);
+        if (getEmailAndPassword()) {
+            m_Auth.signInWithEmailAndPassword(userEmail, userPassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.i("Info", "signInWithEmail:success");
+                                FirebaseUser user = m_Auth.getCurrentUser();
+                                String uid = user.getUid();
+                                ValueEventListener UserListener = new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        User user;
+                                        // Get Post object and use the values to update the UI
+                                        if (dataSnapshot.child("usersProfile").child(uid).exists()) {
+                                            user = dataSnapshot.child("usersProfile").child(uid).getValue(User.class);
+                                            openAllTripsActivity(uid, user);
+                                        }
+
                                     }
 
-                                }
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
 
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
+                                    }
+                                };
+                                mRef.addValueEventListener(UserListener);
 
-                                }
-                            };
-                            mRef.addValueEventListener(UserListener);
+                                //  move to home page
 
-                               //  move to home page
+                            } else {
+                                FirebaseAuthException e = (FirebaseAuthException) task.getException();
+                                Log.e("LoginActivity", "Failed signing in", e);
+                                Context context = getApplicationContext();
+                                CharSequence text = e.getMessage();
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(context, text, duration);
+                                toast.show();
+                                // If sign in fails, display a message to the user.
+                                Log.i("Info", "signInWithEmail:failure", task.getException());
 
-                        } else {
-                            FirebaseAuthException e = (FirebaseAuthException )task.getException();
-                            Log.e("LoginActivity", "Failed signing in", e);
-                            Context context = getApplicationContext();
-                            CharSequence text = e.getMessage();
-                            int duration = Toast.LENGTH_SHORT;
-                            Toast toast = Toast.makeText(context, text, duration);
-                            toast.show();
-                            // If sign in fails, display a message to the user.
-                            Log.i("Info", "signInWithEmail:failure", task.getException());
+                            }
 
                         }
-
-                    }
-                });
+                    });
+        }
+        else
+        {
+            Context context = getApplicationContext();
+            CharSequence text = "You have to enter email and password";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 
     public void onSignUpClicked(View v)
     {
-        getEmailAndPassword();
-        m_Auth.createUserWithEmailAndPassword(userEmail, userPassword)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.i("Info", "createUserWithEmail:success");
-                            FirebaseUser user = m_Auth.getCurrentUser();
-                            String uid = user.getUid();
-                            openSettingsProfileActivity(uid);// move to edit profile
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.i("Info", "createUserWithEmail:failure", task.getException());
-                            FirebaseAuthException e = (FirebaseAuthException )task.getException();
-                            Log.e("LoginActivity", "Failed signing up", e);
-                            Context context = getApplicationContext();
-                            CharSequence text = e.getMessage();
-                            int duration = Toast.LENGTH_SHORT;
-                            Toast toast = Toast.makeText(context, text, duration);
-                            toast.show();
+        if(getEmailAndPassword()) {
+            m_Auth.createUserWithEmailAndPassword(userEmail, userPassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.i("Info", "createUserWithEmail:success");
+                                FirebaseUser user = m_Auth.getCurrentUser();
+                                String uid = user.getUid();
+                                openSettingsProfileActivity(uid);// move to edit profile
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.i("Info", "createUserWithEmail:failure", task.getException());
+                                FirebaseAuthException e = (FirebaseAuthException) task.getException();
+                                Log.e("LoginActivity", "Failed signing up", e);
+                                Context context = getApplicationContext();
+                                CharSequence text = e.getMessage();
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(context, text, duration);
+                                toast.show();
+                            }
                         }
-                    }
-                });
+                    });
+        }
+        else {
+            Context context = getApplicationContext();
+            CharSequence text = "You have to enter email and password";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 
     private void openSettingsProfileActivity(String uid) {
