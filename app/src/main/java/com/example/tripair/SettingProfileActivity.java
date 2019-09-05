7ecb8669dd6d1240b41b7ddb0215032b9541bbf4
rutitka <rutitka@gmail.com>
2019-09-05 15:32:55 +0300

    blulu

--- app/src/main/java/com/example/tripair/SettingProfileActivity.java
+++ app/src/main/java/com/example/tripair/SettingProfileActivity.java
@@ -1,18 +1,12 @@
 package com.example.tripair;
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
